bb9b8ba32c49ae59f32663eb2828a2393787bce6
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-03-29 16:48:54 +0700

    feat: update Profile Page

--- src/features/users/ProfilePage.tsx
+++ src/features/users/ProfilePage.tsx
@@ -1,14 +1,26 @@
+import React from "react";
 import {
+  AlertTitle,
   Box,
   Button,
   InputAdornment,
+  Snackbar,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
 
 import AssignmentIcon from "@mui/icons-material/Assignment";
+
 import Profile from "../../app/models/Profile";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const profiles: Profile[] = [
   {
@@ -24,10 +36,33 @@ const profiles: Profile[] = [
 ];
 
 const ProfilePage = () => {
+  const [open, setOpen] = React.useState(false);
+  const [isSuccess, setIsSuccess] = React.useState(false);
+
+  const handleClick = () => {
+    if (isSuccess) {
+      setIsSuccess(false);
+    } else {
+      setIsSuccess(true);
+    }
+    setOpen(true);
+  };
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <Box sx={{ pl: 40, "& > :not(style)": { m: 1, width: "100ch" } }}>
       <Typography
-        variant="h4"
+        variant="h3"
         gutterBottom
         style={{
           wordWrap: "break-word",
@@ -98,9 +133,32 @@ const ProfilePage = () => {
         }}
       >
         <Stack spacing={2} direction="row">
-          <Button variant="contained">Cancel</Button>
-          <Button variant="contained">Save</Button>
+          <Button variant="contained" onClick={handleClick}>
+            Cancel
+          </Button>
+          <Button variant="contained" onClick={handleClick}>
+            Save
+          </Button>
         </Stack>
+
+        {!isSuccess && (
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="warning">
+              <AlertTitle>Warning</AlertTitle>
+              Inappropriate or malformed data.
+              <strong>Please check and re-enter!</strong>
+            </Alert>
+          </Snackbar>
+        )}
+
+        {isSuccess && (
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success">
+              <AlertTitle>Success</AlertTitle>
+              Your profile has been updated successfully!
+            </Alert>
+          </Snackbar>
+        )}
       </div>
     </Box>
   );
